fb04abaf8448b2bce41ffdbbcc0d480c9c8b863d
fahmi.fahreza <fahmiiireza@gmail.com>
2024-01-08 12:37:28 +0100

    create migration file and model file

--- database/migrations/2022_01_01_000000_create_users_table.php
+++ database/migrations/2022_01_01_000000_create_users_table.php
@@ -4,20 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
+        // Create users table
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->integer('age');
+            $table->string('cv_path')->nullable();
+            $table->enum('role', ['user', 'admin'])->default('user');
             $table->timestamps();
         });
     }
@@ -27,6 +27,7 @@ return new class extends Migration
      */
     public function down(): void
     {
+        // Drop users table if the migration is rolled back
         Schema::dropIfExists('users');
     }
 };

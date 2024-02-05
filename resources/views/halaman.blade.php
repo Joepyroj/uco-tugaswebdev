3bccaec72b9f64e2dbe4ea78a18c84fd8296835d
fahmi.fahreza <fahmiiireza@gmail.com>
2024-02-05 10:30:55 +0100

    add styling

--- resources/views/halaman.blade.php
+++ resources/views/halaman.blade.php
@@ -11,14 +11,12 @@
 <body>
     <header>
         <div>
-            <img src="/path/to/logo.png" alt="Logo Perusahaan" class="logo">
+            <img src="company.png" alt="Logo Perusahaan" class="logo">
             <h1 class="company-name">Job Tracking Website</h1>
         </div>
     </header>
     <main>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, earum consequuntur esse eveniet quos
-            officiis aut, consequatur cumque autem laudantium atque fuga, omnis sed nisi placeat vel blanditiis dolorem
-            eum?</p>
+        <p></p>
         <div class="button-container">
             <a href="{{ route('login') }}"><button>Login</button></a>
             <a href="{{ route('register') }}"><button>Register</button></a>
